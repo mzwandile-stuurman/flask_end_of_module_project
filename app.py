d525fcf237e026891eefa046fcdac0a5afff67d4
mzwandile <stuurmanmzwandile@gmail.com>
2021-08-17 16:35:33 +0200

    cors

--- app.py
+++ app.py
@@ -122,7 +122,6 @@ def welcome_page():
 
 # end-point to register a user
 @app.route('/user-registration/', methods=["POST"])
-@jwt_required
 @cross_origin()
 def user_registration():
     response = {}
@@ -159,7 +158,7 @@ def user_registration():
             return response
 
 @app.route('/user-login/', methods=["POST"])
-@jwt_required()
+
 @cross_origin()
 def user_login():
     response = {}
@@ -195,7 +194,7 @@ def user_login():
 
 # create a product
 @app.route('/create-products/', methods=["POST"])
-@jwt_required # authantication required
+ # authantication required
 @cross_origin()
 def create_Point_of_Sale():
     response = {}
@@ -316,7 +315,7 @@ def delete_product(post_id):
 # get user by password
 @app.route("/get-user-password/<password>", methods=['GET'])
 @cross_origin()
-@jwt_required()
+
 def delete_product_password(password):
     response = {}
     with sqlite3.connect("Point_of_Sale.db") as conn:
@@ -329,7 +328,7 @@ def delete_product_password(password):
 
 
 @app.route("/delete-product-front/", methods=['POST'])
-@jwt_required()
+
 @cross_origin()
 def delete_product_front():
     response = {}
